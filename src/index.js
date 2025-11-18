b1036a36ea137a16996bbee92228eb66f445fb8e
amhg <androzka@yahoo.com>
2025-11-18 13:02:12 +0100

    demo workflow

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ const PORT = process.env.PORT;
 // HTTP GET route we can use to check if the service is handling requests.
 //
 app.get("/live", (req, res) => {
+    console.log('in live')
     res.sendStatus(200);
 });
 
